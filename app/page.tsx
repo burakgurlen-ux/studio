97dac6094bd0705680432e073ee1a28c00410b9e
Adın Soyadın <mail@adresin.com>
2026-02-06 23:49:02 +0300

    sonunda bitti

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Merhaba!</h1>
+      <p>Bu site Burak tarafından başarıyla kuruldu.</p>
+    </main>
+  )
+}
